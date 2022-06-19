2aff074a73bf887169e7e584a4cb996cd82fc98e
river <rrivverr@protonmail.com>
2022-06-19 19:25:50 -0400

    Begin creating objects for game

---
+++ game.js
@@ -0,0 +1,70 @@
+
+
+// Array for tic tac toe board
+
+
+
+
+
+// Player factory ?
+
+
+
+
+// Create player objects
+
+
+
+
+// Create game board
+const gameBoard = (() => {
+
+    const boardSize = 9; 
+
+    let board = [];
+
+    // Generate board
+    const createBoard = (playerCount) => {
+
+        let BoardSquare = (position) => {
+
+            // position 0-8
+            const boardPosition = position;
+    
+            let squareOwner = null; 
+           
+            return {boardPosition, squareOwner}
+        }
+       
+        for (let i = 0; i < boardSize;  i++){ 
+            board[i] = BoardSquare(i)
+        }
+    }
+
+    const setOwner = (pos, player) => {
+        board[pos].squareOwner = player;
+    }
+
+    // Returns square at pos, otherwise entire board
+    const getBoard = (pos) => {
+       return pos != null ? board[pos] : board;
+    }
+    
+    return {createBoard, setOwner, getBoard};
+
+})();
+
+gameBoard.createBoard();
+gameBoard.setOwner(0, 'Player 1')
+console.log(gameBoard.getBoard())
+
+
+// Mark spot (look at color.js)
+
+
+
+
+// Detect winner
+
+
+// Thought --  create object for every row/column that could win.  When an object is filled call a win method??
