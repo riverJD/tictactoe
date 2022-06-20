b27f3f1d3844f762d577a28067cfad7ff2d5c565
river <rrivverr@protonmail.com>
2022-06-20 02:59:12 -0400

    Add logic for choosing squares and switching player turns

--- game.js
+++ game.js
@@ -13,60 +13,105 @@
 
 // Create player objects
 
+const playerObj = {
+
+    //for testing
+    pName: "Player One",
+    pTeam: "X",
+    
+   
+
+}
+const player2Obj = {
+
+    //for testing
+    pName: "Player Two",
+    pTeam: "O",
+    
+   
+
+}
 
 
 // Create game board
 const gameBoard = (() => {
 
-    const boardSize = 9; 
+    const boardSize = 9;
 
-    let board = [];
+    let _playerTurn = playerObj
+    let _board = [];
+
+    const switchPlayersTurn = () => {
+
+            _playerTurn == playerObj? _playerTurn = player2Obj: _playerTurn = playerObj  
+            
+    }
+
+    const getTurn = () => {
+        //onsole.log(_playerTurn);
+        return _playerTurn;
+    }
 
     // Generate board
     const createBoard = (playerCount) => {
 
+        // One gameboard unit
         let BoardSquare = (position) => {
 
             // position 0-8
             const boardPosition = position;
-    
             let squareOwner = null; 
            
             return {boardPosition, squareOwner}
         }
-       
+    
         for (let i = 0; i < boardSize;  i++){ 
-            board[i] = BoardSquare(i)
+            _board[i] = BoardSquare(i)
         }
+
+
     }
 
     const setOwner = (pos, player) => {
-        board[pos].squareOwner = player;
+        _board[pos].squareOwner = player;
     }
 
     // Returns square at pos, otherwise entire board
     const getBoard = (pos) => {
-       return pos != null ? board[pos] : board;
+       return pos != null ? _board[pos] : _board;
     }
     
-    return {createBoard, setOwner, getBoard};
+    return {createBoard, setOwner, getBoard, switchPlayersTurn, getTurn};
 
 })();
 
 gameBoard.createBoard();
-gameBoard.setOwner(0, 'Player 1')
 
 const displayGame = (() => {
 
-    const updateBoard = () => {
-        
-    }
+    const game = document.querySelector('.game');
+    const board = document.querySelector('.game-board');
+    const scoreBoard = document.querySelector('#scoreboard');
+    const turnHeader = document.querySelector('#turn-header');
+    const turnText = document.querySelector('#turn-text')
+
+
+
     const _createSquare = (square) => {
         
-        const board = document.querySelector('.game-board');
+        
         const cell = document.createElement('div');
-        cell.setAttribute('id', `square:${square}`);
-        cell.textContent = square
+        const cellID = document.createElement('div');
+        const cellContent = document.createElement('div');
+        
+    
+        cellContent.classList.add("cell-content");
+        cell.setAttribute('id', square);
+        cellID.textContent = square;
+        cellID.classList.add('cellID')
+        cell.appendChild(cellID);
+        cell.appendChild(cellContent);
+        cell.addEventListener('click', clickSquare.bind(playerObj, cell))
         cell.classList.add('game-square');
         board.appendChild(cell);     
     }   
@@ -75,18 +120,44 @@ const displayGame = (() => {
        
         for (square in size){
             _createSquare(square);
-            
         }
+
     }
     
-    const drawSquare = (position) => {
+    const clickSquare = (cell) => {
+        console.log(gameBoard.getBoard(cell.id).squareOwner);
+        if (gameBoard.getBoard(cell.id).squareOwner != null) {
+            alert("Pick an empty square");
+            return;
+        }
 
+        gameBoard.setOwner(cell.id, gameBoard.getTurn())
+        
+        drawSquare(cell);
+        updateBoard();
+        gameBoard.switchPlayersTurn();
+        
+    }
+
+    const updateBoard = () => {
+        
+        turnText.textContent = gameBoard.getTurn().pName;
+    }
+
+    const drawSquare = (cell) => {
+
+        cell.querySelector('.cell-content').textContent = gameBoard.getBoard(cell.id).squareOwner.pTeam;
+        const cellID = cell.querySelector('.cellID');
+        cellID.textContent = (`${cellID.textContent} (${gameBoard.getTurn().pName})`);
     }
 
     return {generateBoard}
 }
 
 )();
+
+
+
 
 
 
